ab288a9c185f3f4c63d5edc391435203458958ca
yannengfa <940436475@qq.com>
2021-07-02 21:50:05 +0800

    初始化项目

---
+++ src/main/java/com/DemoApplication.java
@@ -0,0 +1,15 @@
+package com;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext ctx = SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/controller/MainController.java
@@ -0,0 +1,14 @@
+package com.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+    @RequestMapping("/")
+    public String index() {
+        System.out.println("/index");
+        return "index";
+    }
+}
